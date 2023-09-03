edea18979c5a8c1550b13f554cd9135c21a15309
unknown <iqro.kristiadi2004@gmail.com>
2023-09-03 23:48:17 +0700

    firs commit

---
+++ routes/cloudinary.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+// middleware
+const {authCheck, adminCheck} = require("../middlewares/auth");
+
+// controller
+const { upload, remove } = require('../controllers/cloudinary')
+
+router.post('/uploadimages', authCheck, adminCheck, upload);
+router.post('/removeimages', authCheck, adminCheck, remove);
+
+module.exports = router;
